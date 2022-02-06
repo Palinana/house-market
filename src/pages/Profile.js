50b22768ac3d537f988912ba1f79783b7b7c2baf
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2022-02-06 15:13:28 -0500

    added authentication and profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuth, updateProfile } from 'firebase/auth';
+
 function Profile () {
+    const auth = getAuth();
+
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+    });
+
+    const { name, email } = formData;
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        auth.signOut();
+        navigate('/');
+    }
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className='profile'>
+            <header className='profileHeader'>
+                <p className='pageHeader'>My Profile</p>
+
+                <button type='button' className='logOut' onClick={onLogout}>
+                    Logout
+                </button>
+            </header>
         </div>
     )
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,123 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {toast} from 'react-toastify';
+
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase.config';
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
+
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
+
 function SignUp () {
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    const {name, email, password} = formData; // in order to use them
+
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+           ...prevState,
+           [e.target.id]: e.target.value
+        })); 
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const auth = getAuth();
+
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+            const user = userCredential.user;
+
+            updateProfile(auth.currentUser, {
+                displayName: name,
+            });
+
+            // copy user data exect the password and add the timestamp
+            const formDataCopy = { ...formData };
+            delete formDataCopy.password;
+            formDataCopy.timestamp = serverTimestamp();
+
+            // save new user data to firestore db
+            await setDoc(doc(db, "users", user.uid), formDataCopy);
+
+            // redirect to explore page
+            navigate("/");
+            
+        } catch (error) {
+            toast.error('Something went wrong with registration')
+        }
+    }
+
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <>
+            <div className="pageContainer">
+                <header>
+                    <p className="pageHeader">
+                        Welcome Back!
+                    </p>
+                </header>
+
+                <form className="pageContainer" onSubmit={onSubmit}>
+                    <input 
+                        type="text" 
+                        className="nameInput" 
+                        placeholder="Name" 
+                        id="name" 
+                        value={name}
+                        onChange={onChange}
+                    />
+
+                    <input 
+                        type="email" 
+                        className="emailInput" 
+                        placeholder="Email" 
+                        id="email" 
+                        value={email}
+                        onChange={onChange}
+                    />
+
+                    <div className="passwordInputDiv">
+                        <input 
+                            type={showPassword ? 'text' : 'password'} 
+                            className="passwordInput" 
+                            placeholder="Password" 
+                            id="password" 
+                            value={password}
+                            onChange={onChange}
+                        />
+                        <img 
+                            src={visibilityIcon} 
+                            alt="show password" 
+                            className="showPassword"
+                            onClick={() => setShowPassword((prevState) => !prevState )}
+                        />
+                    </div>
+
+                    <div className="signUpBar">
+                        <p className="signUpText">Sign Up</p>
+                        <button className="signUpButton">
+                            <ArrowRightIcon fill="#ffffff" width="34px" height="34px"/>
+                        </button>
+                    </div>
+                </form>
+
+                <Link to="/sign-in" className="registerLink">Sign In Instead</Link>
+            </div>
+        </>
     )
 }
 
