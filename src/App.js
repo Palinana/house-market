70c455de4c7fa87821c8564bea04772261b13e35
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2022-02-06 19:13:22 -0500

    added change user details and forgot password

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
 function App () {
     return (
         <>
             <Router>
                 <Routes>
-                    <Route path='/' element={ <Explore/> }/>
-                    <Route path='/offers' element={ <Offers/> }/>
-                    <Route path='/profile' element={ <Profile/> }/>
-                    <Route path='/sign-in' element={ <SignIn/> }/>
-                    <Route path='/sign-up' element={ <SignUp/> }/>
-                    <Route path='/forgot-password' element={ <ForgotPassword/> }/>
+                    <Route path='/' element={<Explore />} />
+                    <Route path='/offers' element={<Offers />} />
+                    <Route path='/profile' element={<PrivateRoute />}>
+                        <Route path='/profile' element={<Profile />} />
+                    </Route>
+                    <Route path='/sign-in' element={<SignIn />} />
+                    <Route path='/sign-up' element={<SignUp />} />
+                    <Route path='/forgot-password' element={<ForgotPassword />} />
                 </Routes>
                 
                 <Navbar />
